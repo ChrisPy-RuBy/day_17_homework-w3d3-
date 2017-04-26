e6311f32553b4fb63dddb7899cc6389147b08bd2
Chris Woodall <christopherhwoodall@gmail.com>
2017-04-26 18:18:52 +0100

    SAVE method for albums and artists

--- db/console.rb
+++ db/console.rb
@@ -1,20 +1,22 @@
 require ('pry-byebug')
-# require_relative('../models/album.rb')
+require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
 
 artist1 = Artist.new({'name' => 'AC/DC'})
 artist2 = Artist.new({'name' => 'Nirvana'})
 artist3 = Artist.new({'name' => 'RATM'})
-# artist1.save()
+artist1.save()
 # artist2.save()
 # artist3.save()
 
-# album1 = Album.new({
-#   'name' => 'Back in black',
-#   'genre' => 'Rock',
-#   'artist_id' => artist1.id
-#   })
+ album1 = Album.new({
+   'name' => 'Back in black',
+   'genre' => 'Rock',
+   'artist_id' => artist1.id
+  })
+
+ album1.save()
 
 # album2 = Album.new({
 #   'name' => 'Nevermind',
