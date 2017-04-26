951cb99a5357cddbd65bd92f11af53b7c7a9e8bb
Chris Woodall <christopherhwoodall@gmail.com>
2017-04-26 18:54:13 +0100

    Methods for album and artist searching

--- db/console.rb
+++ db/console.rb
@@ -7,8 +7,8 @@ artist1 = Artist.new({'name' => 'AC/DC'})
 artist2 = Artist.new({'name' => 'Nirvana'})
 artist3 = Artist.new({'name' => 'RATM'})
 artist1.save()
-# artist2.save()
-# artist3.save()
+artist2.save()
+artist3.save()
 
  album1 = Album.new({
    'name' => 'Back in black',
@@ -16,19 +16,27 @@ artist1.save()
    'artist_id' => artist1.id
   })
 
+ album2 = Album.new({
+   'name' => 'Nevermind',
+   'genre' => 'Grunge',
+   'artist_id' => artist2.id
+   })
+
+ album3 = Album.new({
+   'name' => 'Battle for Los Angeles',
+   'genre' => 'Metal',
+   'artist_id' => artist3.id
+   })
+
+ album4 = Album.new({
+   'name' => 'Evil Empire',
+   'genre' => 'Metal',
+   'artist_id' => artist3.id
+   })
+
  album1.save()
-
-# album2 = Album.new({
-#   'name' => 'Nevermind',
-#   'genre' => 'Grunge',
-#   'artist_id' => artist2.id
-#   })
-
-# album3 = Album.new({
-#   'name' => 'Battle for Los Angeles',
-#   'genre' => 'Metal',
-#   'artist_id' => artist3.id
-#   })
-
+ album2.save()
+ album3.save()
+ album4.save()
 binding.pry
 nil
